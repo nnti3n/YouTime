ce42ca92a1d7cd22944a0a443057838da0436747
Nguyen Nhat Tien <tien@conceptualcode.com>
2016-12-17 18:29:52 +0700

    SearchBar

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
-import SearchBar from './SearchBar.js'
+import SearchBar from './SearchBar'
 
-export const HomeView = () => (
+export const HomeView = (props) => (
   <div>
     <h4>Welcome to YouTime</h4>
     <img
       alt='This is a duck, because Redux!'
       className='duck'
       src={DuckImage} />
-    <SearchBar />
+    <SearchBar SearchVideo={props.SearchVideo} />
   </div>
 )
+
+HomeView.propTypes = {
+  SearchVideo: React.PropTypes.func
+}
 
 export default HomeView
