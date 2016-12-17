dbca83c7c6aea51c5ee9fb4c159ce48938446896
Nguyen Nhat Tien <tien@conceptualcode.com>
2016-12-17 11:25:13 +0700

    Init hot fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,10 @@ import './Header.scss'
 
 export const Header = () => (
   <div>
-    <h1>React Redux Starter Kit</h1>
+    <h1>YouTime from SNVD</h1>
     <IndexLink to='/' activeClassName='route--active'>
       Home
     </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='route--active'>
-      Counter
-    </Link>
   </div>
 )
 
